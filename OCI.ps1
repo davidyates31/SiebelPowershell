bc9cb6b90bdd8bcfdb96981c08ac8ce4300cf5a7
davidyates31 <davidyates31@gmail.com>
2022-03-22 09:21:46 +0000

    powershell

--- OCI.ps1
+++ OCI.ps1
@@ -1,3 +1,50 @@
-Write-Host "Hello"
-Write-Host "Second line"
-Write-Host "Third line"
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$EnvironmentCode,
+    [Parameter(Mandatory=$true)]
+    [string]$Action
+)
+
+function Invoke-StartSiebelServers{
+    $i = 0
+    if(GetSiebelServers){
+        foreach($server in $servers){
+            $i++
+            Write-Output "Starting server "$server.DisplayName
+            # Invoke-OCIComputeInstanceAction -InstanceId $server.OCID -Action $Action.ToUpper()
+            if ($i -ne $servers.Count){
+                Write-Output "Waiting "$server.WaitBetween" seconds before next action"
+                Start-Sleep -Seconds $server.WaitBetween
+            }
+        }
+    }
+}
+
+function Invoke-StopSiebelServers{
+    $i = 0
+    if(GetSiebelServers){
+        $servers = $servers | Sort-Object -Property "Priority" -Descending
+        foreach($server in $servers){
+            $i++
+            Write-Output "Stopping server "$server.DisplayName
+            # Invoke-OCIComputeInstanceAction -InstanceId $server.OCID -Action $Action.ToUpper()
+            if ($i -ne $servers.Count){
+                Write-Output "Waiting "$server.WaitBetween" seconds before next action"
+                Start-Sleep -Seconds $server.WaitBetween
+            }
+        }
+    }
+}
+
+function GetSiebelServers{
+    $ServerList=Import-Csv -Path "C:\VSCode\OCI\servers.csv" 
+    $servers = $ServerList.where({$_.EnvCode -eq $EnvironmentCode})
+    # List of OCIDs for known OCI servers
+    if($servers.Count -eq 0) {
+        Write-Error "Environment $EnvironmentCode is not currently supported" -Category InvalidArgument
+        return $false
+    } else {
+        return $true
+    }
+}
